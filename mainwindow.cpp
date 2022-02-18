d30046431e291c008a0118c0dd89b60c17a6b032
Chunibyo <chunibyo.wly@gmail.com>
2022-02-19 00:36:55 +0800

    add: qt paint event

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,7 +1,25 @@
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow{parent}
-{
+        : QMainWindow{parent} {
+    update();
+}
 
+void MainWindow::paintEvent(QPaintEvent *) {
+    resize(windowWidth, windowHeight);
+    QPainter qpainter(this);
+
+    QColor black("black");
+    QColor white("white");
+    qpainter.setBrush(black);
+    qpainter.setPen(Qt::NoPen);
+    qpainter.drawRect(0, 0, width(), height());
+
+    qpainter.setBrush(white);
+    paintSmallSquare(1, 2, qpainter);
+    paintSmallSquare(2, 3, qpainter);
+}
+
+void MainWindow::paintSmallSquare(int left, int top, QPainter &qpainter) {
+    qpainter.drawRect(QRectF(left * squareSize, top * squareSize, squareSize, squareSize));
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,12 +2,23 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 public:
     explicit MainWindow(QWidget *parent = nullptr);
+
+    void paintEvent(QPaintEvent *) override;
+
+    void paintSmallSquare(int left, int top, QPainter &qpainter);
+
+public:
+    static constexpr int squareSize = 20;
+
+    int windowWidth = 64 * squareSize;
+
+    int windowHeight = 32 * squareSize;
 
 signals:
 
