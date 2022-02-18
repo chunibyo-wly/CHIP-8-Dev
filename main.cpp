1e44d059998910f151b129fd999e43b117264fe1
Chunibyo <chunibyo.wly@gmail.com>
2022-02-18 18:56:51 +0800

    add: qt dependency

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-#include "CHIP8.h"
+#include <QApplication>
 
-int main() {
-    CHIP8 chip8;
-    chip8.init();
-    std::string input = "test_opcode.ch8";
-    chip8.loadROM(input);
-    chip8.run();
+#include "CHIP8.h"
+#include "mainwindow.h"
+
+int main(int argc, char *argv[]) {
+//    CHIP8 chip8;
+//    chip8.init();
+//    std::string input = "/Users/chunibyo/01_project/06_open_source/19_CHIP8/test_opcode.ch8";
+//    chip8.loadROM(input);
+//    chip8.run();
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
